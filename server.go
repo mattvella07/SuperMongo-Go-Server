0525673b684316b736069ce12fbe9ea0e5127c75
Matt Vella <mattvella07@gmail.com>
2016-11-30 08:04:18 -0600

    Using gin instead of standard http package

--- server.go
+++ server.go
@@ -1,15 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "github.com/gin-gonic/gin"
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome!")
+func handler(c *gin.Context) {
+	content := gin.H{"Hello": "World"}
+	c.JSON(200, content)
+}
+
+func getAllDatabases(c *gin.Context) {
+	content := gin.H{"DB": "Mongo"}
+	c.JSON(200, content)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":3001", nil)
+	app := gin.Default()
+	app.GET("/", handler)
+	app.GET("/api/databases", getAllDatabases)
+	app.Run(":3001")
 }
