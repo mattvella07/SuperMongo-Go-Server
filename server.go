9066c6e6e1e6753647c51fa3863f69b1f0e95fb7
Matt Vella <matt.vella@toyotaconnected.com>
2018-09-05 21:00:53 -0500

    Refactor

--- server.go
+++ server.go
@@ -1,117 +1,278 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"strconv"
+	"log"
+	"net/http"
 	"strings"
-	"time"
 
-	"github.com/gin-gonic/gin"
 	"labix.org/v2/mgo"
-	"labix.org/v2/mgo/bson"
 )
 
-//PageLimit represents the max number of mongodb documents to return at one time
-const PageLimit = 20
-
-func handler(c *gin.Context) {
-	content := gin.H{"Test": "Hi"}
-	c.JSON(200, content)
+type listResponse struct {
+	Name string `json:"name"`
 }
 
-func getAllDatabases(c *gin.Context) {
-	m := make(map[string]string)
-	session, err := mgo.Dial("mongodb://localhost:27017/")
+func find(rw http.ResponseWriter, r *http.Request) {
+	// /api/find/:db/:col/:query/:projection/:options
 
+	session, err := mgo.Dial("mongodb://localhost:27017/")
+	defer session.Close()
 	if err != nil {
-		m["Error"] = "Can't connect to database"
-	} else {
-		dbs, dbErr := session.DatabaseNames()
-		if dbErr == nil {
-			for idx, item := range dbs {
-				m[string(idx)] = item
-			}
-		}
-		session.Close()
+		log.Printf("ERROR: %s", err)
+		rw.WriteHeader(500)
+		rw.Write([]byte(fmt.Sprintf("ERROR: %s", err)))
+		return
 	}
 
-	c.JSON(200, m)
+	allParams := strings.Split(strings.Replace(r.URL.String(), "/api/find/", "", -1), "/")
+	var dbName, colName, query, projection, options string
+	if len(allParams) > 0 {
+		dbName = allParams[0]
+	}
+	if len(allParams) > 1 {
+		colName = allParams[1]
+	}
+	if len(allParams) > 2 {
+		query = allParams[2]
+	}
+	if len(allParams) > 3 {
+		projection = allParams[3]
+	}
+	if len(allParams) > 4 {
+		options = allParams[4]
+	}
+
+	fmt.Println("db: ", dbName)
+	fmt.Println("col: ", colName)
+	fmt.Println("query: ", query)
+	fmt.Println("projection: ", projection)
+	fmt.Println("options: ", options)
+
+	db := session.DB(dbName).C(colName)
+	db.Find(query).Limit(PageLimit).All()
+	// 		query := make(map[string]interface{})
+
+	// 		queryParam := c.Param("query")
+	// 		queryParam = strings.Trim(queryParam, "{ }")
+	// 		queryStr := strings.Split(queryParam, ",")
+	// 		for _, q := range queryStr {
+	// 			q = strings.Trim(q, " ")
+	// 			items := strings.Split(q, ":")
+	// 			if len(items) == 2 {
+	// 				//Key and value to be added to the query
+	// 				key := strings.Trim(items[0], "\"")
+	// 				val := items[1]
+
+	// 				//Determine data type of val and convert it
+	// 				if key == "_id" {
+	// 					query[key] = bson.ObjectIdHex(val)
+	// 				} else if intVal, intErr := strconv.Atoi(val); intErr == nil {
+	// 					query[key] = intVal
+	// 				} else if floatVal, floatErr := strconv.ParseFloat(val, 64); floatErr == nil {
+	// 					query[key] = floatVal
+	// 				} else if boolVal, boolErr := strconv.ParseBool(val); boolErr == nil {
+	// 					query[key] = boolVal
+	// 				} else if dateVal, dateErr := time.Parse("2006-01-02", val); dateErr == nil {
+	// 					query[key] = dateVal
+	// 				} else {
+	// 					query[key] = strings.Trim(val, "\"")
+	// 				}
+	// 			}
+	// 		}
+
+	// 		db := session.DB(c.Param("db")).C(c.Param("col"))
+	// 		findErr := db.Find(query).Limit(PageLimit).All(&result)
+	// 		if findErr != nil {
+	// 			fmt.Println("Error: " + findErr.Error())
+	// 		}
+
+	// 		session.Close()
+
+	// 	c.JSON(200, result)
+
 }
 
-func getCollectionsInDB(c *gin.Context) {
-	m := make(map[string]string)
-	session, err := mgo.Dial("mongodb://localhost:27017/")
+func getCollectionsInDB(rw http.ResponseWriter, r *http.Request) {
+	var returnCols []listResponse
 
+	session, err := mgo.Dial("mongodb://localhost:27017/")
+	defer session.Close()
 	if err != nil {
-		m["Error"] = "Can't get collections"
-	} else {
-		db := session.DB(c.Param("db"))
-		cols, colErr := db.CollectionNames()
-		if colErr == nil {
-			for idx, item := range cols {
-				m[string(idx)] = item
-			}
-		}
-		session.Close()
+		log.Printf("ERROR: %s", err)
+		rw.WriteHeader(500)
+		rw.Write([]byte(fmt.Sprintf("ERROR: %s", err)))
+		return
 	}
 
-	c.JSON(200, m)
+	dbName := strings.Replace(strings.Replace(r.URL.String(), "/api/collections/", "", -1), "/", "", -1)
+	fmt.Println(dbName)
+
+	db := session.DB(dbName)
+	cols, err := db.CollectionNames()
+	if err != nil {
+		log.Printf("ERROR: %s", err)
+		rw.WriteHeader(500)
+		rw.Write([]byte(fmt.Sprintf("ERROR: %s", err)))
+		return
+	}
+
+	for _, item := range cols {
+		returnCols = append(returnCols, listResponse{item})
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(returnCols)
 }
 
-func find(c *gin.Context) {
-	var result []bson.M
+func getDatabases(rw http.ResponseWriter, r *http.Request) {
+	var returnDbs []listResponse
+
 	session, err := mgo.Dial("mongodb://localhost:27017/")
-
+	defer session.Close()
 	if err != nil {
-		fmt.Println("Error: " + err.Error())
-	} else {
-		query := make(map[string]interface{})
-
-		queryParam := c.Param("query")
-		queryParam = strings.Trim(queryParam, "{ }")
-		queryStr := strings.Split(queryParam, ",")
-		for _, q := range queryStr {
-			q = strings.Trim(q, " ")
-			items := strings.Split(q, ":")
-			if len(items) == 2 {
-				//Key and value to be added to the query
-				key := strings.Trim(items[0], "\"")
-				val := items[1]
-
-				//Determine data type of val and convert it
-				if key == "_id" {
-					query[key] = bson.ObjectIdHex(val)
-				} else if intVal, intErr := strconv.Atoi(val); intErr == nil {
-					query[key] = intVal
-				} else if floatVal, floatErr := strconv.ParseFloat(val, 64); floatErr == nil {
-					query[key] = floatVal
-				} else if boolVal, boolErr := strconv.ParseBool(val); boolErr == nil {
-					query[key] = boolVal
-				} else if dateVal, dateErr := time.Parse("2006-01-02", val); dateErr == nil {
-					query[key] = dateVal
-				} else {
-					query[key] = strings.Trim(val, "\"")
-				}
-			}
-		}
-
-		db := session.DB(c.Param("db")).C(c.Param("col"))
-		findErr := db.Find(query).Limit(PageLimit).All(&result)
-		if findErr != nil {
-			fmt.Println("Error: " + findErr.Error())
-		}
-
-		session.Close()
+		log.Printf("ERROR: %s", err)
+		rw.WriteHeader(500)
+		rw.Write([]byte(fmt.Sprintf("ERROR: %s", err)))
+		return
 	}
 
-	c.JSON(200, result)
+	dbs, err := session.DatabaseNames()
+	if err != nil {
+		log.Printf("ERROR: %s", err)
+		rw.WriteHeader(500)
+		rw.Write([]byte(fmt.Sprintf("ERROR: %s", err)))
+		return
+	}
+
+	for _, item := range dbs {
+		returnDbs = append(returnDbs, listResponse{item})
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(returnDbs)
+}
+
+func healthCheck(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-Type", "text/html")
+	rw.Write([]byte("Success"))
+}
+
+func createServer() {
+	http.HandleFunc("/", healthCheck)
+	http.HandleFunc("/api/databases", getDatabases)
+	http.HandleFunc("/api/collections/", getCollectionsInDB)
+	http.HandleFunc("/api/find/", find)
+
+	log.Fatal(http.ListenAndServe(":3001", nil))
 }
 
 func main() {
-	app := gin.Default()
-	app.GET("/", handler)
-	app.GET("/api/databases", getAllDatabases)
-	app.GET("/api/collections/:db", getCollectionsInDB)
-	app.GET("/api/find/:db/:col/:query/:projection/:options", find) //Options contains Sort, Limit, and Skip
-	app.Run(":3001")
+	createServer()
 }
+
+// //PageLimit represents the max number of mongodb documents to return at one time
+// const PageLimit = 20
+
+// func handler(c *gin.Context) {
+// 	content := gin.H{"Test": "Hi"}
+// 	c.JSON(200, content)
+// }
+
+// func getAllDatabases(c *gin.Context) {
+// 	m := make(map[string]string)
+// 	session, err := mgo.Dial("mongodb://localhost:27017/")
+
+// 	if err != nil {
+// 		m["Error"] = "Can't connect to database"
+// 	} else {
+// 		dbs, dbErr := session.DatabaseNames()
+// 		if dbErr == nil {
+// 			for idx, item := range dbs {
+// 				m[string(idx)] = item
+// 			}
+// 		}
+// 		session.Close()
+// 	}
+
+// 	c.JSON(200, m)
+// }
+
+// func getCollectionsInDB(c *gin.Context) {
+// 	m := make(map[string]string)
+// 	session, err := mgo.Dial("mongodb://localhost:27017/")
+
+// 	if err != nil {
+// 		m["Error"] = "Can't get collections"
+// 	} else {
+// 		db := session.DB(c.Param("db"))
+// 		cols, colErr := db.CollectionNames()
+// 		if colErr == nil {
+// 			for idx, item := range cols {
+// 				m[string(idx)] = item
+// 			}
+// 		}
+// 		session.Close()
+// 	}
+
+// 	c.JSON(200, m)
+// }
+
+// func find(c *gin.Context) {
+// 	var result []bson.M
+// 	session, err := mgo.Dial("mongodb://localhost:27017/")
+
+// 	if err != nil {
+// 		fmt.Println("Error: " + err.Error())
+// 	} else {
+// 		query := make(map[string]interface{})
+
+// 		queryParam := c.Param("query")
+// 		queryParam = strings.Trim(queryParam, "{ }")
+// 		queryStr := strings.Split(queryParam, ",")
+// 		for _, q := range queryStr {
+// 			q = strings.Trim(q, " ")
+// 			items := strings.Split(q, ":")
+// 			if len(items) == 2 {
+// 				//Key and value to be added to the query
+// 				key := strings.Trim(items[0], "\"")
+// 				val := items[1]
+
+// 				//Determine data type of val and convert it
+// 				if key == "_id" {
+// 					query[key] = bson.ObjectIdHex(val)
+// 				} else if intVal, intErr := strconv.Atoi(val); intErr == nil {
+// 					query[key] = intVal
+// 				} else if floatVal, floatErr := strconv.ParseFloat(val, 64); floatErr == nil {
+// 					query[key] = floatVal
+// 				} else if boolVal, boolErr := strconv.ParseBool(val); boolErr == nil {
+// 					query[key] = boolVal
+// 				} else if dateVal, dateErr := time.Parse("2006-01-02", val); dateErr == nil {
+// 					query[key] = dateVal
+// 				} else {
+// 					query[key] = strings.Trim(val, "\"")
+// 				}
+// 			}
+// 		}
+
+// 		db := session.DB(c.Param("db")).C(c.Param("col"))
+// 		findErr := db.Find(query).Limit(PageLimit).All(&result)
+// 		if findErr != nil {
+// 			fmt.Println("Error: " + findErr.Error())
+// 		}
+
+// 		session.Close()
+// 	}
+
+// 	c.JSON(200, result)
+// }
+
+// func main() {
+// 	app := gin.Default()
+// 	app.GET("/", handler)
+// 	app.GET("/api/databases", getAllDatabases)
+// 	app.GET("/api/collections/:db", getCollectionsInDB)
+// 	app.GET("/api/find/:db/:col/:query/:projection/:options", find) //Options contains Sort, Limit, and Skip
+// 	app.Run(":3001")
+// }
